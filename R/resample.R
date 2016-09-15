edf86addc55ea6b742f650013d3b1519ba6db09f
Damjan Vukcevic <damjan@vukcevic.net>
2016-09-15 23:22:35 +1000

    Use roxygen2 tags for more complete documentation
    This corresponds to an example given in the talk slides.  The Details section
    was omitted due to space constraints on the slide.

--- R/resample.R
+++ R/resample.R
@@ -2,11 +2,16 @@
 #'
 #' \code{resample} takes a random sample from a given vector.
 #'
-#' This function is an alternative to \code{sample}.  Unlike the latter, it
-#' always interprets the first argument as the set of elements to sample from,
-#' even when it is of length 1.  This consistency makes it more suitable for
-#' programmatic use.
-#'
 #' @export
+#' @param x   A vector of elements from which to sample.
+#' @param ... Further arguments passed to \code{sample}.
+#'
+#' @return A vector of elements drawn from \code{x}.
+#'
+#' @seealso \code{\link[base]{sample}}
+#'
+#' @examples
+#' resample(5:10)  # same as sample(5:10)
+#' resample(5)     # NOT the same as sample(5)
 resample <- function(x, ...)
     x[sample.int(length(x), ...)]
