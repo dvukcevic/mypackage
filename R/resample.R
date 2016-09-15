f8a3e4f25d00c8eb14cff8a7e3cfb78249848373
Damjan Vukcevic <damjan@vukcevic.net>
2016-09-15 23:25:10 +1000

    Restore the Details section
    This is done here for completeness.

--- R/resample.R
+++ R/resample.R
@@ -2,6 +2,11 @@
 #'
 #' \code{resample} takes a random sample from a given vector.
 #'
+#' This function is an alternative to \code{sample}.  Unlike the latter, it
+#' always interprets the first argument as the set of elements to sample from,
+#' even when it is of length 1.  This consistency makes it more suitable for
+#' programmatic use.
+#'
 #' @export
 #' @param x   A vector of elements from which to sample.
 #' @param ... Further arguments passed to \code{sample}.
